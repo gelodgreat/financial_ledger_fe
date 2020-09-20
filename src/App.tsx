ee95ae13c8218a182ab42751d47a1c1aa0cdd28b
Angelo Umali <carions46@gmail.com>
2020-09-20 11:30:47 +0800

    Added types. added sample login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,79 @@
 import React, { Component } from 'react';
-import {Switch, Route, withRouter} from 'react-router-dom';
-import Login from './components/Login';
-import {FirebaseAuthProvider, FirebaseAuthConsumer} from '@react-firebase/auth';
+
+import {
+  FirebaseAuthProvider,
+  FirebaseAuthConsumer,
+} from '@react-firebase/auth';
+import { ConnectedRouter } from 'connected-react-router';
 import firebase from 'firebase/app';
 import config from 'Firebase/config';
-import {FirebaseGoogleLogin} from './Firebase/functions';
-
-import { ConnectedRouter } from 'connected-react-router';
-import { BrowserRouter, Route, Switch, Router } from 'react-router-dom';
+import {
+  Switch,
+  Route,
+  withRouter,
+  BrowserRouter,
+  Router,
+} from 'react-router-dom';
 import Routes from 'routes/index.routes';
 import { history } from 'store';
 
+import Login from './components/Login';
+import { FirebaseGoogleLogin } from './Firebase/functions';
 import logo from './logo.svg';
 
 import './App.css';
 
-interface Props{
+interface Props {}
 
-}
-
-class App extends Component{
-
-  render(){
+class App extends Component {
+  render() {
     console.log(this);
-    return(
+    return (
       <Switch>
-
         <FirebaseAuthProvider {...config} firebase={firebase}>
           <FirebaseAuthConsumer>
-            {({user, providerId})=>
-            <>
-              {providerId ? 
-              (
+            {({ user, providerId }) => (
               <>
-              {!user &&(
-                <Route
-                  render={routeProps=><Login/>}
-                />
-              )}
-              {user &&(
-                <Route
-                render={routeProps=><Route
-                  render={routeProps=><div className="App">
-                  <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                      Edit <code>src/App.tsx</code> and save to reload.
-                    </p>
-                    <a
-                      className="App-link"
-                      href="https://reactjs.org"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      Learn React
-                    </a>
-                  </header>
-                </div>}
-                />}
-              />
-              )}
-              </>) : (<p>Loading...</p>)}
-            </>
-            }
-
+                {providerId ? (
+                  <>
+                    {!user && <Route render={(routeProps) => <Login />} />}
+                    {user && (
+                      <Route
+                        render={(routeProps) => (
+                          <Route
+                            render={(routeProps) => (
+                              <div className="App">
+                                <header className="App-header">
+                                  <img
+                                    src={logo}
+                                    className="App-logo"
+                                    alt="logo"
+                                  />
+                                  <p>
+                                    Edit <code>src/App.tsx</code> and save to
+                                    reload.
+                                  </p>
+                                  <a
+                                    className="App-link"
+                                    href="https://reactjs.org"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                  >
+                                    Learn React
+                                  </a>
+                                </header>
+                              </div>
+                            )}
+                          />
+                        )}
+                      />
+                    )}
+                  </>
+                ) : (
+                  <p>Loading...</p>
+                )}
+              </>
+            )}
           </FirebaseAuthConsumer>
         </FirebaseAuthProvider>
       </Switch>
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,36 +1,41 @@
-import React,{Component} from 'react';
-import {FirebaseAuthProvider, FirebaseAuthConsumer} from '@react-firebase/auth';
+import React, { Component } from 'react';
+
+import {
+  FirebaseAuthProvider,
+  FirebaseAuthConsumer,
+} from '@react-firebase/auth';
 import firebase from 'firebase/app';
 import config from 'Firebase/config';
-import {FirebaseGoogleLogin} from '../Firebase/functions';
 
-
+import { FirebaseGoogleLogin } from '../Firebase/functions';
 
 class Login extends Component {
-
-  render(){
+  render() {
     return (
-      <FirebaseAuthProvider {...config} firebase={firebase} >
+      <FirebaseAuthProvider {...config} firebase={firebase}>
         <FirebaseAuthConsumer>
-          {({user, providerId})=>
-          <>
-          {providerId ?(
+          {({ user, providerId }) => (
             <>
-            {user &&(
-              <p>Welcome {user.displayName}</p>
-            )}
-            {!user &&(
-              <>
-                <form>
-                  <button>Submit</button>
-                </form>
-                <button onClick={()=>FirebaseGoogleLogin()}>Login with Google</button>
-              </>
-            )}
+              {providerId ? (
+                <>
+                  {user && <p>Welcome {user.displayName}</p>}
+                  {!user && (
+                    <>
+                      <form onSubmit={() => {}}>
+                        <input></input>
+                        <button type="submit">Submit</button>
+                      </form>
+                      <button onClick={() => FirebaseGoogleLogin()}>
+                        Login with Google
+                      </button>
+                    </>
+                  )}
+                </>
+              ) : (
+                <p>Loading...</p>
+              )}
             </>
-          ):(<p>Loading...</p>)}
-          </>
-          }
+          )}
         </FirebaseAuthConsumer>
       </FirebaseAuthProvider>
     );
