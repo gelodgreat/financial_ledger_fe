b75f32b397c6968357f9850b55a10251d1231fc9
Angelo Umali <carions46@gmail.com>
2020-09-20 13:15:02 +0800

    Fix proper routing

--- src/Firebase/config.ts
+++ src/Firebase/config.ts
@@ -1,11 +1,11 @@
-import "firebase/auth";
+import 'firebase/auth';
 export default {
-    apiKey: "AIzaSyDuDcqcH0eKNWE1Y90Q-5CwjLKH_IsAJlo",
-    authDomain: "financial-budgeting-a8d47.firebaseapp.com",
-    databaseURL: "https://financial-budgeting-a8d47.firebaseio.com",
-    projectId: "financial-budgeting-a8d47",
-    storageBucket: "financial-budgeting-a8d47.appspot.com",
-    messagingSenderId: "589339173436",
-    appId: "1:589339173436:web:0082961d35d810070a1939",
-    measurementId: "G-WQNRLG19VF"
-  };
+  apiKey: 'AIzaSyDuDcqcH0eKNWE1Y90Q-5CwjLKH_IsAJlo',
+  authDomain: 'financial-budgeting-a8d47.firebaseapp.com',
+  databaseURL: 'https://financial-budgeting-a8d47.firebaseio.com',
+  projectId: 'financial-budgeting-a8d47',
+  storageBucket: 'financial-budgeting-a8d47.appspot.com',
+  messagingSenderId: '589339173436',
+  appId: '1:589339173436:web:0082961d35d810070a1939',
+  measurementId: 'G-WQNRLG19VF',
+};
--- src/routes/index.routes.tsx
+++ src/routes/index.routes.tsx
@@ -24,15 +24,15 @@ export const ROUTES: Routes = {
     children: <h1>Landing Page</h1>,
   },
   LOGIN: {
-    path: APP_ROUTES.LOGIN,
+    path: MAIN_ROUTES.LOGIN,
     children: <Login />,
   },
   REGISTER: {
-    path: APP_ROUTES.REGISTER,
+    path: MAIN_ROUTES.REGISTER,
     children: <h1>Register</h1>,
   },
   FORGOT_PASSWORD: {
-    path: APP_ROUTES.FORGOT_PASSWORD,
+    path: MAIN_ROUTES.FORGOT_PASSWORD,
     children: <h1>Forgot Password</h1>,
   },
   //nested routes
@@ -45,24 +45,7 @@ const RoutesComponent = (): JSX.Element => {
   const history = useHistory();
   // const location = useLocation();
   // const currentPath = location.pathname;
-  const isAuthenticated = false;
   return (
-    // <Switch>
-    //   {Object.values(ROUTES).map((route) => {
-    //     return (
-    //       <Route key={route.path} path={route.path} exact={!route.nested}>
-    //         {!isAuthenticated && route.protected ? (
-    //           <Redirect to={{ pathname: APP_ROUTES.LOGIN }} />
-    //         ) : (
-    //           route.children
-    //         )}
-    //       </Route>
-    //     );
-    //   })}
-    //   <Route>
-    //     <h1>404 route</h1>
-    //   </Route>
-    // </Switch>
     <Switch>
       <FirebaseAuthProvider {...config} firebase={firebase}>
         <FirebaseAuthConsumer>
@@ -76,7 +59,7 @@ const RoutesComponent = (): JSX.Element => {
                     exact={!route.nested}
                   >
                     {!user && !providerId ? (
-                      <Redirect to={{ pathname: APP_ROUTES.LOGIN }} />
+                      <Redirect to={{ pathname: MAIN_ROUTES.LANDING }} />
                     ) : (
                       route.children
                     )}
